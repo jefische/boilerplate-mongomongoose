78370a78c8c79b1ed496ea888a9d6d86b0db7478
Jeremy Fischer <jefische86@gmail.com>
2024-12-10 14:29:08 -0500

    Completed all challenges

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 let mongoose = require('mongoose');
 
-// useNewUrlParser and useUnifiedTopology are deprecated with at least version 8.8.4
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// useNewUrlParser and useUnifiedTopology are deprecated with version 8.8.4
 // mongoose.connect(process.env.MONGO_URI);
 
 const Schema = mongoose.Schema;
@@ -22,12 +22,12 @@ const createAndSavePerson = (done) => {
 	
 	// Apparently versions of mongoose past 5.11 no longer accept callback functions for Model.prototype.save
 
-	// janeFonda.save( (err, data) => {
-	// 	if (err) return done(err);
-	// 	done(null, data);
-	// 	console.log(data + ' - is the data print');
-	// })
-	janeFonda.save();
+	janeFonda.save( (err, data) => {
+		if (err) return done(err);
+		done(null, data);
+		console.log(data + ' - is the data print');
+	})
+	// janeFonda.save();
 	// console.log('test my console log'); note this does print to the terminal when I npm run start and pass the challenge on FCC.
 };
 
@@ -70,32 +70,58 @@ const findPersonById = (personId, done) => {
 };
 
 // (8) Perform classic updates by running find, edit, then save
-const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
+const findEditThenSave = function(personId, done) {
+	const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+	Person.findById(personId, function(err, person) {
+
+		if(err) return console.log(err);
+
+		person.favoriteFoods.push(foodToAdd);
+
+		person.save(function(err, updatedPerson) {
+			if (err) return console.log(err)
+			done(null, updatedPerson);
+		})
+	})
 };
 
+// (9)  Perform new updates on a document using model.findOneAndUpdate()
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, function(err, updatedDoc) {
+	if (err) return console.log(err);
+	done(null, updatedDoc);
+	})
 };
 
+// (10) Delete one document using model.findByIdAndRemove
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+	Person.findByIdAndRemove(personId, (err, person) => {
+		if (err) return console.log(err);
+		done(null, person);
+	})
 };
 
+// (11) Delete many documents with model.remove()
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove}, (err, response) => {
+	if (err) return console.log(err);
+	done(null, response);
+  })
 };
 
+// (12) Chain search query helpers to narrow search results
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({age: 0}).exec(function(err, response) {
+	if (err) return console.log(err);
+	done(null, response);
+  })
 };
 
 /** **Well Done !!**
